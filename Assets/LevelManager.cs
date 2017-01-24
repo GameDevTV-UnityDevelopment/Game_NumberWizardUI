f2956ff00bf510e03c5f9b7e89c0160c06d49e16
Rob Meade <baldy.meade@gmail.com>
2017-01-24 13:18:57 +0000

    Project Initial Commit
    + Base version of Number Wizard UI

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the level with the specified name
+    /// </summary>
+    /// <param name="name">The name of the level to load</param>
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for : " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    /// <summary>
+    /// Quits the game
+    /// </summary>
+    /// <remarks>Limited functionality on Web and Debug/Editor builds, bad practice for mobile devices</remarks>
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard: MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+
+    public int maxGuessesAllowed = 5;
+
+    public Text text;
+
+    /// <summary>
+    /// Initialise
+    /// </summary>
+    void Start()
+    {
+        StartGame();
+    }
+
+    /// <summary>
+    /// Start the game
+    /// </summary>
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+
+        NextGuess();
+    }
+
+    /// <summary>
+    /// Guesses a higher number
+    /// </summary>
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    /// <summary>
+    /// Guesses a lower number
+    /// </summary>
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    /// <summary>
+    /// Calculates the next guess
+    /// </summary>
+    void NextGuess()
+    {
+        guess = Random.Range(min, max+1);
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+
+        if(maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
